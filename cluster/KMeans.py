a22e52e24a01b349c071fa30fd5faec77fc93f93
Pedro Buarque Santos <buarqu.p@wustl.edu>
2017-03-25 17:09:44 -0300

    commit

---
+++ cluster/KMeans.py
@@ -0,0 +1,57 @@
+import numpy as np 
+from math import sqrt
+ 
+class KMeans(object):
+	
+	def __init__(self, n_clusters=2, n_iter=300, shuffle=True, tolerance=0.001):
+		self.n_iter = n_iter
+		self.n_clusters = n_clusters
+		self.tolerance = tolerance
+		self.shuffle = shuffle
+		self.run = False
+		
+ 	def fit(self, x, y):
+ 		self.run = True
+ 		self.centroids = {}
+
+ 		if self.shuffle:
+ 			r = np.random.permutation(len(y))
+ 			for k in range(len(r[:self.n_clusters])):
+ 				self.centroids[k] = x[r[k]]
+ 		else:
+ 			for k in range(self.n_clusters):
+ 				self.centroids[k] = x[k]
+
+ 		for itr in range(self.n_iter):
+ 			self.clusters = {}
+
+ 			for k in range(self.n_clusters):
+ 				self.clusters[k] = []
+
+ 			for xi in x:
+ 				dist = [np.linalg.norm(xi - self.centroids[c]) for c in self.centroids]
+ 				class_ = dist.index(min(dist))
+ 				self.clusters[class_].append(xi)
+
+ 			old_centroids = dict(self.centroids)
+ 			for k in self.clusters:
+ 				self.centroids[k] = np.average(self.clusters[k], axis=0)
+
+ 			is_done = True
+ 			for k in self.centroids:
+ 				old_centroid = old_centroids[k]
+ 				centroid = self.centroids[k]
+ 				if (np.linalg.norm(old_centroid - centroid) > self.tolerance):
+ 					is_done = False
+
+ 			if is_done:
+ 				break
+
+	def predict(self, x):
+		if self.run:
+			class_ = []
+			for c in self.centroids:
+				class_.append(np.sum((x - self.centroids[c]) ** 2, axis=1))
+ 			return np.argmin(np.array(class_).T, axis=1)
+		else:
+			raise Exception("NonTrainedModelException: You must fit data first!")
---
+++ optimization/PSO.py
@@ -0,0 +1,15 @@
+import numpy as np 
+ 
+class PSO(object):
+	
+	def __init__(self):
+ 		pass
+
+ 	def fit(self):
+ 		pass
+
+	def optimize(self):
+		pass
+
+	def score(self):
+		pass
