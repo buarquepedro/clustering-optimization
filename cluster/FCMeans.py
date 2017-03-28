eefd9a75e08ddb7b3fa230ccce443757421e368d
Pedro Buarque Santos <buarqu.p@wustl.edu>
2017-03-28 00:43:12 -0300

    FCMeans

--- cluster/FCMeans.py
+++ cluster/FCMeans.py
@@ -2,14 +2,76 @@ import numpy as np
  
 class FCMeans(object):
 	
-	def __init__(self):
- 		pass
+	def __init__(self, n_clusters=3, n_iter=300, shuffle=True, fuzzy_c=0.5, tolerance=0.0001):
+		self.n_clusters = n_clusters
+		self.n_iter = n_iter
+		self.fuzzy_c = fuzzy_c
+		self.tolerance = tolerance
+		self.shuffle = shuffle
+		self.run = False
 
  	def fit(self, x, y):
- 		pass
+ 		self.run = True
+ 		self.centroids = {}
+
+ 		if len(x.shape) < 1:
+ 			raise Exception("DataException: Dataset must contain more examples" + 
+ 							"than the required number of clusters!")
+ 		if self.shuffle:
+ 			r = np.random.permutation(len(y))
+ 			for k in range(len(r[:self.n_clusters])):
+ 				self.centroids[k] = x[r[k]]
+ 		else:
+ 			for k in range(self.n_clusters):
+ 				self.centroids[k] = x[k]
+
+ 		self.degree_of_membership = {}
+ 		for idx, xi in enumerate(x):
+ 			weights = np.array([np.random.random() for i in range(0,self.n_clusters)])
+ 			weights = weights/np.sum(weights)
+ 			self.degree_of_membership[idx] = weights
+
+ 		for iterations in range(self.n_iter):
+ 			powers = {}
+ 			for idx in self.degree_of_membership:
+ 				powers[idx] = np.power(self.degree_of_membership[idx], self.fuzzy_c)
+
+ 			for idx_ in self.centroids:
+ 				centroid = []
+ 				sum_membeship = 0
+ 				for idx, xi in enumerate(x):
+ 					centroid.append(self.degree_of_membership[idx][idx_] * np.array(xi))
+ 					sum_membeship += self.degree_of_membership[idx][idx_]
+ 				centroid = np.sum(centroid, axis=0)
+ 				centroid = centroid/sum_membeship
+ 				self.centroids[idx_] = centroid
+
+ 			max_episilon = 0.0
+ 			for idx_ in self.centroids:
+ 				for idx, xi in enumerate(x):
+ 					updated_degree_of_membership = 0.0
+ 					norm = np.linalg.norm(xi - self.centroids[idx_])
+ 					all_norms = [norm/np.linalg.norm(xi - self.centroids[c]) for c in self.centroids]
+ 					all_norms = np.power(all_norms, 2/(self.fuzzy_c-1))
+ 					updated_degree_of_membership = 1/sum(all_norms)
+ 					diff = updated_degree_of_membership - self.degree_of_membership[idx][idx_]
+
+ 					if diff > max_episilon:
+ 						max_episilon = diff
+
+ 			if max_episilon < self.tolerance:
+ 				break
 
 	def predict(self, x):
-		pass
-
-	def score(self, x, y):
-		pass
+		if self.run:
+			if len(x.shape) > 1:
+				class_ = []
+				for c in self.centroids:
+					class_.append(np.sum((x - self.centroids[c]) ** 2, axis=1))
+	 			return np.argmin(np.array(class_).T, axis=1)
+	 		else:
+	 			dist = [np.linalg.norm(x - self.centroids[c]) for c in self.centroids]
+ 				class_ = dist.index(min(dist))
+ 				return class_
+		else:
+			raise Exception("NonTrainedModelException: You must fit data first!")
---
+++ demo_fcmeans.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from cluster import FCMeans
+
+def main():
+	df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+	y = df.iloc[:, 4].values
+	y[y == "Iris-setosa"] = 0
+	y[y == "Iris-versicolor"] = 1
+	y[y == "Iris-virginica"] = 2
+	x = df.iloc[:, [0,2]].values
+
+	x = np.array(x)
+	y = np.array(y)
+
+	x[:, 0] = (x[:, 0] - x[:, 0].mean())/x[:, 0].std()
+	x[:, 1] = (x[:, 1] - x[:, 1].mean())/x[:, 1].std()
+
+	clf = FCMeans.FCMeans(n_clusters=2)
+	clf.fit(x,y)
+
+	print clf.centroids
+	
+	colors = ['g', 'm', 'y']
+	for idx in range(len(np.unique(y))):
+		plt.scatter(x[:, 0][y == idx], x[:, 1][y == idx], c=colors[idx], s=10, edgecolors='k')
+
+	for k in clf.centroids:
+		plt.scatter(clf.centroids[k][0], clf.centroids[k][1], marker='x', s=100, c='b')
+
+	plt.title('Fuzzy C-Means')
+	plt.xlabel('sepal length [standardized]')
+	plt.ylabel('petal length [standardized]')
+	plt.legend(loc='upper left')
+	plt.show()
+
+if __name__ == '__main__':
+	main()
