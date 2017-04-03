47bee502402600c05f5f7dbc759b68596c46444c
Pedro Buarque Santos <buarqu.p@wustl.edu>
2017-04-02 23:17:07 -0300

    pso

--- demo_pso.py
+++ demo_pso.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from optimization import PSO
 
 def main():
-	opt = PSO.PSO(dim=5, minf=-5.12, maxf=5.12, swarm_size=30, n_iter=10000, w=0.8, w_damp=0.99, c1=2.05, c2=2.05)
+	opt = PSO.PSO(dim=30, minf=-5.12, maxf=5.12, swarm_size=30, n_iter=10000, w=0.8, w_damp=0.99, c1=2.05, c2=2.05)
 	opt.optimize('sphere')
 
 	plt.plot([(i+1) for i in range(len(opt.best_cost))], opt.best_cost)
 	plt.title('Particle Swarm Optimization')
+	plt.legend(["Sphere"])
 	plt.xlabel('Iterations')
 	plt.ylabel('Fitness')
 	plt.show()
