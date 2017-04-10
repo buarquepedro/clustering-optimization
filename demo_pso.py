14ae5616c8c952b722ab6e320236768339a4d270
Pedro Buarque Santos <buarqu.p@wustl.edu>
2017-04-10 00:33:03 -0300

    pso

--- demo_pso.py
+++ demo_pso.py
@@ -4,22 +4,22 @@ from optimization.PSO import PSO
 from optimization.ObjectiveFunction import ObjectiveFunction
 
 def main():
-	opt1 = PSO(dim=30, minf=-100, maxf=100, swarm_size=30, n_iter=100, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
+	opt1 = PSO(dim=30, minf=-100, maxf=100, min_init=50, max_init=100, swarm_size=50, n_iter=1000, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
 	opt1.optimize(ObjectiveFunction.sphere_function)
 
-	opt2 = PSO(dim=30, minf=-30, maxf=30, swarm_size=30, n_iter=100, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
+	opt2 = PSO(dim=30, minf=-30, maxf=30, min_init=15, max_init=30, swarm_size=50, n_iter=1000, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
 	opt2.optimize(ObjectiveFunction.rosenbrock_function)
 
-	opt3 = PSO(dim=30, minf=-5.12, maxf=5.12, swarm_size=30, n_iter=100, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
+	opt3 = PSO(dim=30, minf=-5.12, maxf=5.12, min_init=2.56, max_init=5.12, swarm_size=50, n_iter=1000, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
 	opt3.optimize(ObjectiveFunction.rastrigin_function)
 
-	opt4 = PSO(dim=30, minf=-100, maxf=100, swarm_size=30, n_iter=100, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
+	opt4 = PSO(dim=30, minf=-100, maxf=100, min_init=50, max_init=100, swarm_size=50, n_iter=1000, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
 	opt4.optimize(ObjectiveFunction.schwefel_function)
 
-	opt5 = PSO(dim=30, minf=-600, maxf=600, swarm_size=30, n_iter=100, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
+	opt5 = PSO(dim=30, minf=-600, maxf=600, min_init=300, max_init=600, swarm_size=50, n_iter=1000, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
 	opt5.optimize(ObjectiveFunction.griewank_function)
 
-	opt6 = PSO(dim=30, minf=-32, maxf=32, swarm_size=30, n_iter=100, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
+	opt6 = PSO(dim=30, minf=-32, maxf=32, min_init=16, max_init=32, swarm_size=50, n_iter=1000, w=0.8, lb_w=0.4, w_damp=0.99, c1=2.05, c2=2.05)
 	opt6.optimize(ObjectiveFunction.ackley_function)
 
 	plt.subplot(231)
--- optimization/PSO.py
+++ optimization/PSO.py
@@ -11,19 +11,20 @@ class Particle(object):
  
 class PSO(object):
         
-    def __init__(self, dim, minf, maxf, swarm_size=100, n_iter=500, w=1, lb_w=0.2, w_damp=0.99, c1=2, c2=2, v_max=None, hard_state=True):
+    def __init__(self, dim, minf, maxf, min_init, max_init, swarm_size=100, n_iter=500, w=1, lb_w=0.2, w_damp=0.99, c1=2, c2=2, v_max=None):
         self.dim = dim
         self. minf = minf
         self.maxf = maxf
+        self.min_init = min_init
+        self.max_init = max_init
         self.swarm_size = swarm_size
         self.n_iter = n_iter
         self.w = w
         self.lb_w = lb_w
-        self.w_damp = w_damp
+        self.w_damp = self.w - self.lb_w
         self.c1 = c1
         self.c2 = c2
         self.global_optimum = np.inf
-        self.hard_state = hard_state
         if v_max is None:
             self.v_max = 0.5*np.absolute(maxf - minf)
 
@@ -34,12 +35,8 @@ class PSO(object):
 
         for i in range(self.swarm_size):
             particle = Particle(self.dim)
-            if self.hard_state:
-                p = np.random.randint(self.dim)
-                a = np.random.uniform(self.minf, 0.8*self.minf, p)
-                b = np.random.uniform(0.8*self.maxf, self.maxf, self.dim-p)
-                particle.pos = np.concatenate((a,b), axis=0)
-                particle.pos = particle.pos[np.random.permutation(len(particle.pos))]
+            if (self.min_init is not None) and (self.max_init is not None):
+                particle.pos = np.random.uniform(self.min_init, self.max_init, self.dim)
             else:
                 particle.pos = np.random.uniform(self.minf, self.maxf, self.dim)
             particle.cost = self.evaluate(particle.pos, func_type)
@@ -85,4 +82,4 @@ class PSO(object):
                     self.global_optimum.cost = p.best_cost
                     
             if (self.w > self.lb_w):
-                self.w = self.w * self.w_damp
+                self.w = (i/self.n_iter)*self.w_damp
